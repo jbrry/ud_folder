e40381f0be61fdd6dbc15beea00cf2ee7d5a0904
Jbar-ry <james.barry26@mail.dcu.ie>
2018-07-26 15:48:28 +0100

    changed dir structure, added functionality to use mixed model

--- udpipe_test.sh
+++ udpipe_test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-UDPIPE_MODEL_DIR=~/ud_folder/udpipe_baseline/models
+UDPIPE_MODEL_DIR=~/ud_folder/models    # need to download and extract models from: http://universaldependencies.org/conll18/baseline.html
+MIXED_MODEL_STR="mixed-ud"             # a mixed UD model is used for certain languages which have no training data
+
 
 for tb in ~/ud_folder/ud-treebanks-v2.2/*/*-ud-test.conllu; do
   dir=`dirname ${tb}`            # e.g. /home/james/ud_folder/ud-treebanks-v2.2/UD_Afrikaans-AfriBooms  
@@ -18,14 +20,17 @@ for tb in ~/ud_folder/ud-treebanks-v2.2/*/*-ud-test.conllu; do
   code=${code%%-*}        # e.g. af_afribooms
   
   for text_file in ${code}/${code}*.txt; do 
-    echo ${text_file}
-
-   ## Work in progres ##
-   udpipe --tokenize --tag --parse "${UDPIPE_MODEL_DIR}"/"${model_str}"-ud-2.2-conll18-180430.udpipe ${text_file} > "$code/$code-udpipe.conllu"
-
-   done
+    echo "Now working on -- " ${text_file}
+    if [[ -n $(find "${UDPIPE_MODEL_DIR}/" -name "${model_str}"*.udpipe) ]]
+    then
+        echo "Found UDPipe model for ${model_str} "
+        udpipe --tokenize --tag --parse "${UDPIPE_MODEL_DIR}"/"${model_str}"-ud-2.2-conll18-180430.udpipe ${text_file} > "$code/$code-udpipe.conllu"
+    else 
+	echo "No UDPipe model found for ${model_str}, using mixed UDPipe model"
+        udpipe --tokenize --tag --parse "${UDPIPE_MODEL_DIR}"/"${MIXED_MODEL_STR}"-ud-2.2-conll18-180430.udpipe ${text_file} > "$code/$code-udpipe.conllu"
+    fi 
+    done
 done 
 
-# ensure the use of the right params. 
-
+# do we need to call out params at test time?
 
